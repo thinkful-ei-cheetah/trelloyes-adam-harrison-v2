c9ec5e93e7051ba6badc3c6abaf25ad063b1f132
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-15 15:40:53 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './App';
 import './index.css';
 import {STORE} from './store';
 
-ReactDOM.render(<App 
-store={STORE}
-/>, document.getElementById('root'));
+ReactDOM.render(
+<App store={STORE}/>,
+ document.getElementById('root'));
